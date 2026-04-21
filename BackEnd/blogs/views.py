82bd09e769081062d5c2cf7118c525eb0a29768f
AKJilani <akjilani691995@gmail.com>
2026-04-21 11:35:13 +0600

    final clean

--- BackEnd/blogs/views.py
+++ BackEnd/blogs/views.py
@@ -6,27 +6,30 @@ from .serializers import BlogCategorySerializer, BlogSerializer, BlogCommentSeri
 from rest_framework.parsers import MultiPartParser, FormParser
 from django_filters.rest_framework import DjangoFilterBackend
 
+
 class IsOwnerOrReadOnly(permissions.BasePermission):
-	def has_object_permission(self, request, view, obj):
-		if request.method in permissions.SAFE_METHODS:
-			return True
-		owner = getattr(obj, "author", None) or getattr(obj, "user", None)
-		return owner == request.user
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        owner = getattr(obj, "author", None) or getattr(obj, "user", None)
+        return owner == request.user
+
 
 class BlogCategoryViewSet(viewsets.ModelViewSet):
-	queryset = BlogCategory.objects.all().order_by("name")
-	serializer_class = BlogCategorySerializer
-	permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-	filter_backends = [filters.SearchFilter, filters.OrderingFilter]
-	search_fields = ["name","description"]
-	ordering_fields = ["name","created_at"]
+    queryset = BlogCategory.objects.all().order_by("name")
+    serializer_class = BlogCategorySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ["name", "description"]
+    ordering_fields = ["name", "created_at"]
+
 
 class BlogViewSet(viewsets.ModelViewSet):
-    queryset = Blog.objects.select_related("author","category").all().order_by("-created_at")
+    queryset = Blog.objects.select_related("author", "category").all().order_by("-created_at")
     serializer_class = BlogSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     filter_backends = [filters.SearchFilter, filters.OrderingFilter, DjangoFilterBackend]  # Add DjangoFilterBackend
-    search_fields = ["title","description","content"]
+    search_fields = ["title", "description", "content"]
     ordering_fields = ["created_at"]
     filterset_fields = ['category']  # Add this line
     parser_classes = [MultiPartParser, FormParser]
@@ -38,13 +41,14 @@ class BlogViewSet(viewsets.ModelViewSet):
         blog.save(update_fields=["view_count"])
         return Response({"view_count": blog.view_count})
 
-class BlogCommentViewSet(viewsets.ModelViewSet):
-	serializer_class = BlogCommentSerializer
-	permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
-	def get_queryset(self):
-		qs = BlogComment.objects.select_related("blog","user").all().order_by("created_at")
-		blog_id = self.request.query_params.get("blog")
-		if blog_id:
-			qs = qs.filter(blog_id=blog_id)
-		return qs 
+class BlogCommentViewSet(viewsets.ModelViewSet):
+    serializer_class = BlogCommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        qs = BlogComment.objects.select_related("blog", "user").all().order_by("created_at")
+        blog_id = self.request.query_params.get("blog")
+        if blog_id:
+            qs = qs.filter(blog_id=blog_id)
+        return qs
